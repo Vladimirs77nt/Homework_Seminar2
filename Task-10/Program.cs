1f119823bc08c4b903fe454dd9bdb4456ff0554f
Vladimirs77nt <vladimirs77@yandex.ru>
2022-10-05 07:54:44 +0500

    Первое домашнее задание готово - Задача 10

---
+++ Task-10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int secondDigit = (number / 10);
+
+if (number >= 100 && number <= 999)
+    {
+    secondDigit = number/10 ; // (781/10) = 78
+    secondDigit = secondDigit%10 ; // 78%10 = 8
+    Console.WriteLine($"{number} -> {secondDigit}");
+    }
+else
+    Console.WriteLine("вы ввели неверное число!");
